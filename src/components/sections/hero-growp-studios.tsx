7f5ee706881c25c7366b7e62272928b4b97bf33e
Thales Gregorio <137453849+thales-gsd@users.noreply.github.com>
2026-05-27 12:05:42 -0300

    add growp-lang to home hero

--- src/components/sections/hero-growp-studios.tsx
+++ src/components/sections/hero-growp-studios.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export default function HeroGrowpStudios() {
   return (
-    <div className="w-full mt-[100px] h-[calc(60vh)] border rounded-lg flex flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl font-semibold">Growp Studios...</h1>
-      <p>in development</p>
+    <div className="w-full mt-[100px] h-[calc(60vh)] border rounded-lg overflow-hidden relative">
+      <iframe
+        src="https://growplang.vercel.app/"
+        title="Growp Lang"
+        className="w-full h-full border-none"
+      />
+      <Link
+        href="https://growplang.vercel.app/"
+        target="_blank"
+        className="absolute inset-0 z-10"
+      />
     </div>
   );
 }
